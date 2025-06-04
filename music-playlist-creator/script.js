1bfcbabd1bf67925e5edcd445b43395f9340a239
Ben Snyder <bensnyder@meta.com>
2025-06-04 16:38:16 -0700

    jazz up popup

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,7 +1,18 @@
 
 let popupBtn = document.querySelector("button");
-let popupElem = document.querySelector(".popup");
+let popupElem = document.querySelector("#greyBox");
+let closeBtn = document.querySelector(".close-btn");
+
 
 popupBtn.addEventListener("click", function() { /* display the popup */
     popupElem.classList.toggle("hide");
+    //greyPopup.classList.toggle("hide");
 })
+
+
+
+
+closeBtn.addEventListener("click", function() {
+    popupElem.classList.toggle("hide");
+    //greyPopup.classList.toggle("hide");
+});
