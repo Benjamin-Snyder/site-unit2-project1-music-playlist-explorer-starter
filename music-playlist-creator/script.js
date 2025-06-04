cf3acb11a7e3ac7ad72cad386901747ed831bd6d
Ben Snyder <bensnyder@meta.com>
2025-06-04 14:04:09 -0700

    got a rough draft of popup

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,7 @@
 
+let popupBtn = document.querySelector("button");
+let popupElem = document.querySelector(".popup");
+
+popupBtn.addEventListener("click", function() { /* display the popup */
+    popupElem.classList.toggle("hide");
+})
