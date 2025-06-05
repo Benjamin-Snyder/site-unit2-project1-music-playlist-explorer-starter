6a647c9208fedcf5fb7b6f1c50df94bd0056fa13
Ben Snyder <bensnyder@meta.com>
2025-06-04 17:32:28 -0700

    add some photos

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,14 +1,14 @@
 
-let popupBtn = document.querySelector("button");
+let popupBtn = document.querySelectorAll(".box");
 let popupElem = document.querySelector("#greyBox");
 let closeBtn = document.querySelector(".close-btn");
 
-
-popupBtn.addEventListener("click", function() { /* display the popup */
-    popupElem.classList.toggle("hide");
-    //greyPopup.classList.toggle("hide");
-})
-
+popupBtn.forEach(box => {
+    box.addEventListener("click", function() { /* display the popup */
+        popupElem.classList.toggle("hide");
+        //greyPopup.classList.toggle("hide");
+    })
+});
 
 
 
