eba1438880e810c050d84137f5e00c5bd4690c6a
Ben Snyder <bensnyder@meta.com>
2025-06-05 08:57:21 -0700

    add more photos and connect path to data

--- data/data.js
+++ data/data.js
@@ -3,304 +3,304 @@ let playlists = [
     playlistID: 1,
     playlist_name: "Chill Vibes",
     playlist_author: "DJ Chilly",
-    playlist_art: "./assets/images/chill_vibes.jpg",
+    playlist_art: "./assets/img/free-photo-of-breathtaking-coastal-view-in-puerto-de-la-cruz.jpeg",
     songs: [
         {
         songName: "Banana Pancakes",
         artist: "Jack Johnson",
         duration: "3:12",
-        albumCover: "./assets/images/banana_pancakes.jpg",
+        albumCover: "./assets/img/free-photo-of-a-distance-sign-at-a-port-in-john-ogroats-scotland.jpeg"
         },
         {
         songName: "Sunset Lover",
         artist: "Petit Biscuit",
         duration: "3:58",
-        albumCover: "./assets/images/sunset_lover.jpg",
+        albumCover: "./assets/img/free-photo-of-a-woman-in-a-white-dress-lying-on-the-beach.jpeg"
         },
         {
         songName: "Ocean Eyes",
         artist: "Billie Eilish",
         duration: "3:20",
-        albumCover: "./assets/images/ocean_eyes.jpg",
+        albumCover: "./assets/img/free-photo-of-barbary-macaque-on-the-rock-of-gibraltar.jpeg"
         },
         {
         songName: "Lost in Japan",
         artist: "Shawn Mendes",
         duration: "3:21",
-        albumCover: "./assets/images/lost_in_japan.jpg",
+        albumCover: "./assets/img/free-photo-of-charming-trulli-houses-in-alberobello-italy.jpeg"
         },
         {
         songName: "Electric Feel",
         artist: "MGMT",
         duration: "3:49",
-        albumCover: "./assets/images/electric_feel.jpg",
-        },
-    ],
+        albumCover: "./assets/img/free-photo-of-couple-admiring-scenic-mountain-view-outdoors.jpeg"
+        }
+    ]
     },
     {
     playlistID: 2,
     playlist_name: "Workout Pump",
     playlist_author: "FitBeats",
-    playlist_art: "./assets/images/workout_pump.jpg",
+    playlist_art: "./assets/img/free-photo-of-empty-beach-with-a-lifeguard-tower.jpeg",
     songs: [
         {
         songName: "Stronger",
         artist: "Kanye West",
         duration: "5:11",
-        albumCover: "./assets/images/stronger.jpg",
+        albumCover: "./assets/img/free-photo-of-intricate-stone-archway-in-historical-architecture.jpeg"
         },
         {
         songName: "Eye of the Tiger",
         artist: "Survivor",
         duration: "4:05",
-        albumCover: "./assets/images/eye_of_the_tiger.jpg",
+        albumCover: "./assets/img/free-photo-of-plants-in-pots-by-the-window.jpeg"
         },
         {
         songName: "Can't Hold Us",
         artist: "Macklemore & Ryan Lewis",
         duration: "4:18",
-        albumCover: "./assets/images/cant_hold_us.jpg",
+        albumCover: "./assets/img/free-photo-of-rustic-bread-and-fruit-on-a-dining-table.jpeg"
         },
         {
         songName: "Remember The Name",
         artist: "Fort Minor",
         duration: "3:50",
-        albumCover: "./assets/images/remember_the_name.jpg",
+        albumCover: "./assets/img/free-photo-of-scenic-mediterranean-village-with-sea-view.jpeg"
         },
         {
         songName: "Power",
         artist: "Kanye West",
         duration: "4:52",
-        albumCover: "./assets/images/power.jpg",
-        },
-    ],
+        albumCover: "./assets/img/free-photo-of-scenic-view-of-boat-on-ocean-from-carvoeiro-cave.jpeg"
+        }
+    ]
     },
     {
     playlistID: 3,
     playlist_name: "Classic Rock",
     playlist_author: "RockMaster",
-    playlist_art: "./assets/images/classic_rock.jpg",
+    playlist_art: "./assets/img/free-photo-of-scenic-view-of-lake-bled-with-church.jpeg",
     songs: [
         {
         songName: "Stairway to Heaven",
         artist: "Led Zeppelin",
         duration: "8:02",
-        albumCover: "./assets/images/stairway_to_heaven.jpg",
+        albumCover: "./assets/img/free-photo-of-scenic-view-of-lake-through-leafy-archway.jpeg"
         },
         {
         songName: "Bohemian Rhapsody",
         artist: "Queen",
         duration: "5:55",
-        albumCover: "./assets/images/bohemian_rhapsody.jpg",
+        albumCover: "./assets/img/free-photo-of-scenic-view-of-south-stack-lighthouse-at-dusk.jpeg"
         },
         {
         songName: "Hotel California",
         artist: "Eagles",
         duration: "6:30",
-        albumCover: "./assets/images/hotel_california.jpg",
+        albumCover: "./assets/img/free-photo-of-stunning-view-of-half-dome-in-yosemite-national-park.jpeg"
         },
         {
         songName: "Sweet Child O' Mine",
         artist: "Guns N' Roses",
         duration: "5:56",
-        albumCover: "./assets/images/sweet_child_o_mine.jpg",
+        albumCover: "./assets/img/free-photo-of-sumatran-orangutan-climbing-in-indonesian-forest.jpeg"
         },
         {
         songName: "Back In Black",
         artist: "AC/DC",
         duration: "4:15",
-        albumCover: "./assets/images/back_in_black.jpg",
-        },
-    ],
+        albumCover: "./assets/img/free-photo-of-young-woman-reading-newspaper-in-artistic-setting.jpeg"
+        }
+    ]
     },
     {
     playlistID: 4,
     playlist_name: "Pop Hits",
     playlist_author: "TopCharts",
-    playlist_art: "./assets/images/pop_hits.jpg",
+    playlist_art: "./assets/img/pexels-photo-11734026.jpeg",
     songs: [
         {
         songName: "Blinding Lights",
         artist: "The Weeknd",
         duration: "3:20",
-        albumCover: "./assets/images/blinding_lights.jpg",
+        albumCover: "./assets/img/pexels-photo-12715153.jpeg"
         },
         {
         songName: "Levitating",
         artist: "Dua Lipa",
         duration: "3:23",
-        albumCover: "./assets/images/levitating.jpg",
+        albumCover: "./assets/img/pexels-photo-29875053.jpeg"
         },
         {
         songName: "Shape of You",
         artist: "Ed Sheeran",
         duration: "3:53",
-        albumCover: "./assets/images/shape_of_you.jpg",
+        albumCover: "./assets/img/pexels-photo-30613412.jpeg"
         },
         {
         songName: "Don't Start Now",
         artist: "Dua Lipa",
         duration: "3:03",
-        albumCover: "./assets/images/dont_start_now.jpg",
+        albumCover: "./assets/img/pexels-photo-31976170.jpeg"
         },
         {
         songName: "Watermelon Sugar",
         artist: "Harry Styles",
         duration: "2:54",
-        albumCover: "./assets/images/watermelon_sugar.jpg",
-        },
-    ],
+        albumCover: "./assets/img/pexels-photo-31977035.jpeg"
+        }
+    ]
     },
     {
     playlistID: 5,
     playlist_name: "Jazz Essentials",
     playlist_author: "SmoothJazz",
-    playlist_art: "./assets/images/jazz_essentials.jpg",
+    playlist_art: "./assets/img/pexels-photo-30613412.jpeg",
     songs: [
         {
         songName: "So What",
         artist: "Miles Davis",
         duration: "9:22",
-        albumCover: "./assets/images/so_what.jpg",
+        albumCover: "./assets/img/pexels-photo-31977035.jpeg"
         },
         {
         songName: "Take Five",
         artist: "Dave Brubeck",
         duration: "5:24",
-        albumCover: "./assets/images/take_five.jpg",
+        albumCover: "./assets/img/pexels-photo-11734026.jpeg"
         },
         {
         songName: "My Favorite Things",
         artist: "John Coltrane",
         duration: "13:41",
-        albumCover: "./assets/images/my_favorite_things.jpg",
+        albumCover: "./assets/img/pexels-photo-12715153.jpeg"
         },
         {
         songName: "Feeling Good",
         artist: "Nina Simone",
         duration: "2:53",
-        albumCover: "./assets/images/feeling_good.jpg",
+        albumCover: "./assets/img/pexels-photo-31976170.jpeg"
         },
         {
         songName: "What a Wonderful World",
         artist: "Louis Armstrong",
         duration: "2:19",
-        albumCover: "./assets/images/wonderful_world.jpg",
-        },
-    ],
+        albumCover: "./assets/img/pexels-photo-29875053.jpeg"
+        }
+    ]
     },
     {
     playlistID: 6,
     playlist_name: "Hip-Hop Classics",
     playlist_author: "RapGod",
-    playlist_art: "./assets/images/hiphop_classics.jpg",
+    playlist_art: "./assets/img/free-photo-of-barbary-macaque-on-the-rock-of-gibraltar.jpeg",
     songs: [
         {
         songName: "Juicy",
         artist: "The Notorious B.I.G.",
         duration: "5:02",
-        albumCover: "./assets/images/juicy.jpg",
+        albumCover: "./assets/img/free-photo-of-plants-in-pots-by-the-window.jpeg"
         },
         {
         songName: "Nuthin' But A 'G' Thang",
         artist: "Dr. Dre",
         duration: "4:01",
-        albumCover: "./assets/images/g_thang.jpg",
+        albumCover: "./assets/img/free-photo-of-rustic-bread-and-fruit-on-a-dining-table.jpeg"
         },
         {
         songName: "Lose Yourself",
         artist: "Eminem",
         duration: "5:26",
-        albumCover: "./assets/images/lose_yourself.jpg",
+        albumCover: "./assets/img/free-photo-of-couple-admiring-scenic-mountain-view-outdoors.jpeg"
         },
         {
         songName: "C.R.E.A.M.",
         artist: "Wu-Tang Clan",
         duration: "4:12",
-        albumCover: "./assets/images/cream.jpg",
+        albumCover: "./assets/img/free-photo-of-intricate-stone-archway-in-historical-architecture.jpeg"
         },
         {
         songName: "California Love",
         artist: "2Pac",
         duration: "4:45",
-        albumCover: "./assets/images/california_love.jpg",
-        },
-    ],
+        albumCover: "./assets/img/free-photo-of-scenic-view-of-south-stack-lighthouse-at-dusk.jpeg"
+        }
+    ]
     },
     {
     playlistID: 7,
     playlist_name: "Electronic Beats",
     playlist_author: "DJ Electro",
-    playlist_art: "./assets/images/electronic_beats.jpg",
+    playlist_art: "./assets/img/free-photo-of-stunning-view-of-half-dome-in-yosemite-national-park.jpeg",
     songs: [
         {
         songName: "One More Time",
         artist: "Daft Punk",
         duration: "5:20",
-        albumCover: "./assets/images/one_more_time.jpg",
+        albumCover: "./assets/img/pexels-photo-31976170.jpeg"
         },
         {
         songName: "Animals",
         artist: "Martin Garrix",
         duration: "5:03",
-        albumCover: "./assets/images/animals.jpg",
+        albumCover: "./assets/img/pexels-photo-11734026.jpeg"
         },
         {
         songName: "Titanium",
         artist: "David Guetta ft. Sia",
         duration: "4:05",
-        albumCover: "./assets/images/titanium.jpg",
+        albumCover: "./assets/img/pexels-photo-12715153.jpeg"
         },
         {
         songName: "Clarity",
         artist: "Zedd ft. Foxes",
         duration: "4:31",
-        albumCover: "./assets/images/clarity.jpg",
+        albumCover: "./assets/img/pexels-photo-29875053.jpeg"
         },
         {
         songName: "Levels",
         artist: "Avicii",
         duration: "5:39",
-        albumCover: "./assets/images/levels.jpg",
-        },
-    ],
+        albumCover: "./assets/img/free-photo-of-empty-beach-with-a-lifeguard-tower.jpeg"
+        }
+    ]
     },
     {
     playlistID: 8,
     playlist_name: "Indie Faves",
     playlist_author: "IndieLover",
-    playlist_art: "./assets/images/indie_faves.jpg",
+    playlist_art: "./assets/img/free-photo-of-charming-trulli-houses-in-alberobello-italy.jpeg",
     songs: [
         {
         songName: "Dog Days Are Over",
         artist: "Florence + The Machine",
         duration: "4:13",
-        albumCover: "./assets/images/dog_days_are_over.jpg",
+        albumCover: "./assets/img/free-photo-of-sumatran-orangutan-climbing-in-indonesian-forest.jpeg"
         },
         {
         songName: "Electric Feel",
         artist: "MGMT",
         duration: "3:49",
-        albumCover: "./assets/images/electric_feel.jpg",
+        albumCover: "./assets/img/free-photo-of-a-woman-in-a-white-dress-lying-on-the-beach.jpeg"
         },
         {
         songName: "Young Folks",
         artist: "Peter Bjorn and John",
         duration: "4:39",
-        albumCover: "./assets/images/young_folks.jpg",
+        albumCover: "./assets/img/free-photo-of-scenic-mediterranean-village-with-sea-view.jpeg"
         },
         {
         songName: "Pumped Up Kicks",
         artist: "Foster The People",
         duration: "4:00",
-        albumCover: "./assets/images/pumped_up_kicks.jpg",
+        albumCover: "./assets/img/free-photo-of-scenic-view-of-boat-on-ocean-from-carvoeiro-cave.jpeg"
         },
         {
         songName: "Take Me Out",
         artist: "Franz Ferdinand",
         duration: "3:57",
-        albumCover: "./assets/images/take_me_out.jpg",
-        },
-    ],
-    },
+        albumCover: "./assets/img/free-photo-of-breathtaking-coastal-view-in-puerto-de-la-cruz.jpeg"
+        }
+    ]
+    }
 ];
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,9 +1,9 @@
 
-let popupBtn = document.querySelectorAll(".box");
+let boxes = document.querySelectorAll(".box");
 let popupElem = document.querySelector("#greyBox");
 let closeBtn = document.querySelector(".close-btn");
 
-popupBtn.forEach(box => {
+boxes.forEach(box => {
     box.addEventListener("click", function() { /* display the popup */
         popupElem.classList.toggle("hide");
         //greyPopup.classList.toggle("hide");
@@ -16,3 +16,10 @@ closeBtn.addEventListener("click", function() {
     popupElem.classList.toggle("hide");
     //greyPopup.classList.toggle("hide");
 });
+
+
+function importAlbums(){
+    for (box in boxes) {
+        
+    }
+}
